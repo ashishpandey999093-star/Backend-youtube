7962c2e9720364383e977f711abe4a53b7793420
ashish_0002 <ashishpandey999093@gmail.com>
2026-07-23 14:56:54 +0530

    routes and controllers config for user

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import dotenv from "dotenv";
 import connectDB from "./db/index.js"
 import { app } from "./app.js";
  
+dotenv.config({
+    path:'./env'
+})
 
 connectDB()
 .then(()=>{
-    app.on("error",(error)=>{
-        console.log("Error: ",error);
-        throw error;
-    })
+
     app.listen(process.env.PORT||4000,()=>{
         console.log(`server  is running at port: ${process.env.PORT||4000}`);
     })
---
+++ src/routes/user.routes.js
@@ -0,0 +1,11 @@
+import {Router} from "express";
+import { registerUser } from "../controllers/user.controller.js";
+
+const userRouter=Router();
+
+userRouter.route("/register").post(registerUser)
+// userRouter.route("/login").post(login)
+
+
+
+export default userRouter;
